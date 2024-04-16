2282b76b7e65283d34d30bb5f012b0f59eba4505
Felipe Nunes <164574699+Coto-nete@users.noreply.github.com>
2024-04-16 19:50:14 -0300

    Update 001.go

--- lista02/001.go
+++ lista02/001.go
@@ -2,37 +2,40 @@ package main
 import("fmt")
 
 func main(){
-	var matricula int 
-	ftm.Scan(&matricula)
-	if matricula == -1{
-		return
-	}
-	var temp,mp,ml,mt float32
-	for c:=0;c<8;c++{
-		fmt.Scan(&temp)
-		mp +=temp
-	}
-	mp /=8
-	for c:=0;c<5;c++{
-		fmt.Scan(&temp)
-		ml +=temp
-	}
-	ml /=5
-	fmt.Scan(&temp)
-	nt += temp
-	var pres float32
-	fmt.Scan(&pres)
-	if (0.7*mp + 0.15*ml +0.15*nt>6){
-		if pres>75/100{
-			fmt.printf("Aprovado")
-		}else{
-			fmt.printf("Reprovado por frequencia ")
+	l:=1
+	for l<5{
+		var matricula int 
+		fmt.Scan(&matricula)
+		if matricula == -1{
+			return
 		}
-	}else{
-		if pres>75/100{
-			fmt.printf("Reprovado por nota ")
+		var temp,mp,ml,nt float32
+		for c:=0;c<8;c++{
+			fmt.Scan(&temp)
+			mp +=temp
+		}
+		mp /=8
+		for c:=0;c<5;c++{
+			fmt.Scan(&temp)
+			ml +=temp
+		}
+		ml /=5
+		fmt.Scan(&temp)
+		nt += temp
+		var pres float32
+		fmt.Scan(&pres)
+		if (0.7*mp + 0.15*ml +0.15*nt>6){
+			if pres>96{
+				fmt.Printf("Matricula : %v, Nota final: %.2f,Situação final:Aprovado\n",matricula,nt)
+			}else{
+				fmt.Printf("Matricula : %v, Nota final: %.2f,Situação final:Reprovado por frequencia\n ",matricula,nt)
+			}
 		}else{
-			fmt.printf("Reprovado por nota e frequencia ")
+			if pres>96{
+				fmt.Printf("Matricula : %v, Nota final: %.2f,Situação final:Reprovado por nota \n",matricula,nt)
+			}else{
+				fmt.Printf("Matricula : %v, Nota final: %.2f,Situação final:Reprovado por nota e frequencia \n",matricula,nt)
+			}
 		}
 	}
 }
