97ee1f19671ba7be55ba62e2ef18727870e7a320
Felipe Nunes <felipe_nunes@discente.ufg.br>
2024-05-03 00:22:13 -0300

    .

--- lista03/022.go
+++ lista03/022.go
@@ -1,19 +1,8 @@
 package main
-import("fmt")
+import ("fmt")
 
 func main(){
-	var i,f int
-	var ent,temp string 
-	for {
-		fmt.Scan(&i)
-		fmt.Scan(&f)
-		if i ==0 ||f==0 {
-			break
-		}
-		fmt.Scan(&ent)
-		for c:=i-f;c>=i;c++{
-			temp += ent[c]
-		}
-		fmt.Print(temp)
-	}
+	var in,fm int 
+	var reg []int
+	
 }
